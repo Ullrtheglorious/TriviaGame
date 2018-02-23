2ce6cf37f8fe18211a24cea960cd50481a2a8057
Corey Richard <ullrtheglorious@gmail.com>
2018-02-22 22:12:44 -0700

    fixed correct selction

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,43 +13,43 @@ var trivia= {
         sound[0].play();
     },
 
-    timeLeft: 5,
+    timeLeft: 30,
 
     questionArray: [
         {
             question: "A Sphinx, which is seen guarding the front of the Khafres Pyramid, is made up of which two distinguishing features?",
             answers: {
-                a: { text: "Half Lion, Half Human", correct: true}, 
-                b: { text: "Half Snake, Half Human", correct: false }, 
-                c: { text: "Half Tiger, Half Human", correct: false }, 
+                a: { text: "A: Half Lion, Half Human", correct: true}, 
+                b: { text: "B: Half Snake, Half Human", correct: false }, 
+                c: { text: "C: Half Tiger, Half Human", correct: false }, 
             },
         },{
             question: "The Kelpie, a shape shifting water spirit inhabits local lochs and pools of Scotland, what shape is it more commonly known to appear as before luring victims to their death?",
             answers:{
-                a: { text: "Dragon", correct: false },
-                b: { text: "Dolphin", correct: false },
-                c: { text: "Horse", correct: true },
+                a: { text: "A: Dragon", correct: false },
+                b: { text: "B: Dolphin", correct: false },
+                c: { text: "C: Horse", correct: true },
             },
         },{
             question: "Described as a female spirit in Irish Mythology who usually shrieks, or wailes to killt their vicitms, is known as what?",
             answers:{
-                a: { text: "Bannik", correct: false },
-                b: { text: "Baba Yaga", correct: false },
-                c: { text: "Banshee", correct: true },
+                a: { text: "A: Bannik", correct: false },
+                b: { text: "B: Baba Yaga", correct: false },
+                c: { text: "C: Banshee", correct: true },
             },
         },{
             question: "A Doppelganger is also known as what?",
             answers: {
-                a:{ text: "An evil twin",  correct: true },
-                b:{ text: "A second cousin", correct: false },
-                c:{ text: "An unfamiliar person", correct: false },
+                a:{ text: "A: An evil twin",  correct: true },
+                b:{ text: "B: A second cousin", correct: false },
+                c:{ text: "C: An unfamiliar person", correct: false },
             },
         },{
             question: "A Wendigo,which is native to the nortern forests of the Atlantic Coast and Great Lakes Region, commonly eats what?",
             answers:{
-                a:{ text: "Household pets", correct: false },
-                b:{ text: "Humans", correct: true },
-                b:{ text: "Leftover food left out", correct: false},
+                a:{ text: "A: Household pets", correct: false },
+                b:{ text: "B: Humans", correct: true },
+                c:{ text: "C: Leftover food left out", correct: false},
             },
         }
     ],
@@ -72,31 +72,42 @@ var trivia= {
                 if (trivia.timeLeft == 0) {
                     trivia.endSound();
                     timeOut();
-                    trivia.questionImput();
+                    trivia.questionInput();
                     trivia.startGame(start());
                     $('#display').text("5")
-                    trivia.timeLeft = 5;
+                    trivia.timeLeft = 30;
                 }
             }
             function timeOut() {
                 clearInterval(intervalId);
             }
             start();
-            trivia.questionImput();
+            trivia.questionInput();
         })
     },
     
-    questionImput: function() {
+    questionInput: function() {
         pickRandom = Math.floor(Math.random() * trivia.questionArray.length);
         randomQuestion = trivia.questionArray[pickRandom];
         $("#questionDisplay").html(randomQuestion.question);
         $("#answerA").html(randomQuestion.answers.a.text);
+        $("#answerA").attr("correct", randomQuestion.answers.a.correct);
         $("#answerB").html(randomQuestion.answers.b.text);
+        $("#answerB").attr("correct", randomQuestion.answers.b.correct);
         $("#answerC").html(randomQuestion.answers.c.text);
+        $("#answerC").attr("correct", randomQuestion.answers.c.correct);
     },
 
     answerSelect: function() {
-        $('.answers').click(function() {
+        $(".answers").click(function() {
+            console.log(this);
+            var val = $(this).attr("correct")
+            if (val == "true") {
+                alert("correct");
+                trivia.questionInput();
+            } else {
+                alert("wrong");
+            }
             
         })
 
@@ -121,3 +132,4 @@ var trivia= {
 
 
 trivia.onLoad();
+trivia.answerSelect();
